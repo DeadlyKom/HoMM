9d4a311a32d40d9a8621ffd7bebc3f78e0e9e0c9
DeadlyKom <gma.deadly.kom@mail.ru>
2026-03-17 23:38:25 +0300

    - найдена ошибка при пропуске половинки гексагона, не проверялся верхняя граница видимой части экрана

--- Source/World/Render/Object/ObjectDirtyEnvironment.asm
+++ Source/World/Render/Object/ObjectDirtyEnvironment.asm
@@ -31,25 +31,6 @@ DirtyEnvir:     ; инициализация
                 JR Z, .NextObject                                               ; переход, если флаг не установлен
                 PUSH DE
 
-                ; расчёт положения объекта относительно верхнего-левого видимойго края
-                ; CALL Utilities.TransformToScr
-                ; LD (Kernel.Utilities.SpriteBound.PositionX), DE
-                ; LD (Kernel.Utilities.SpriteBound.PositionY), HL
-
-                ; ; определение способа отображения объекта
-                ; LD A, (IY + FObjectDefaultSettings.Class)
-                ; AND OBJECT_CLASS_MASK
-
-                ; ; ловушка
-                ; ifdef _DEBUG
-                ; CP OBJECT_CLASS_MAX
-                ; DEBUG_BREAK_POINT_NC                                            ; ошибка, нет такого объекта
-                ; endif
-
-                ; LD HL, .JumpTable
-                ; CALL Func.JumpTable
-                ; JR C, .Failed                                                   ; переход, если не удалось определить bound спрайта
-
                 ;   DE - позиция спрайта в пикселях (D - y, E - x)
                 ;   BC - размер bound спрайта в пикселях (B - y, C - x)
                 LD DE, (IY + FObject.Bound + FSpriteBound.Location)
@@ -79,14 +60,6 @@ DirtyEnvir:     ; инициализация
 .RET            SCF                                                             ; флаг для таблицы вызова,
                                                                                 ; говорит что bound спрайта неопределён
                 RET
-
-.JumpTable      DW Hero.Bound                                                   ; OBJECT_CLASS_HERO
-                DW .RET                                                         ; OBJECT_CLASS_CONSTRUCTION
-                DW .RET                                                         ; OBJECT_CLASS_PROPS
-                DW .RET                                                         ; OBJECT_CLASS_INTERACTION
-                DW .RET                                                         ; OBJECT_CLASS_PARTICLE
-                DW .RET                                                         ; OBJECT_CLASS_DECAL
-                DW .RET                                                         ; OBJECT_CLASS_UI
 
                 display " - Object dirty environment:\t\t\t\t", /A, DirtyEnvir, "\t= busy [ ", /D, $-DirtyEnvir, " byte(s)  ]"
 
