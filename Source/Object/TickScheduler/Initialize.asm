deda898af0112e375a740179d175e6bc7037e31e
DeadlyKom <gma.deadly.kom@mail.ru>
2026-07-04 12:43:01 +0300

    добавлен кадровый барьер планировщика тика

--- Source/Object/TickScheduler/Initialize.asm
+++ Source/Object/TickScheduler/Initialize.asm
@@ -9,7 +9,12 @@
 ; Note:
 ;   необходимо включить страницу работы с объектами (страница 0)
 ; ----------------------------------------
-Initialize:     ; порядок чанков по умолчанию: 0..35
+Initialize:     ; инициализация кадрового барьера
+                LD A, (TickCounterRef)
+                DEC A
+                LD (TickObjectChunk.LastFrame), A                               ; гарантирует первый запуск Executor
+
+                ; порядок чанков по умолчанию: 0..35
                 LD HL, TickScheduler.Variables + FTickScheduler.ChunkOrder
                 LD B, MAX_WORLD_CHUNK_SIZE
                 XOR A
